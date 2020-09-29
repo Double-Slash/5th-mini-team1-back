82ad17af8c4beed957d4a99f3c4cd3343e8acee6
Ubuntu <doubleslash@double.vyeujo31oz3edajh45ha0asfwg.syx.internal.cloudapp.net>
2020-09-29 14:52:17 +0000

    index file change

--- where/views.py
+++ where/views.py
@@ -5,10 +5,11 @@ from django.template import loader
 import datetime
 
 def ping(request):
-    msg = datetime.datetime.now()
+    time = datetime.datetime.now()
+    msg = str(time)
     # template = loader.get_template('where/index.html')
     # context = {
     #     'latest_question_list': latest_question_list,
     # }
     # return HttpResponse(template.render(context, request))
-    return HttpResponse(msg, content_type='text/plain')
+    return HttpResponse(time, content_type='text/plain')
